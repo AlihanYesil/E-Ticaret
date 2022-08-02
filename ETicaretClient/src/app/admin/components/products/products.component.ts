bf4f964cec1ce0e7f5affb0d97b4477dbedc84fa
Alihan Yesil <alihannyesil@gmail.com>
2022-08-02 13:39:03 +0300

    product list and pagination

--- ETicaretClient/src/app/admin/components/products/products.component.ts
+++ ETicaretClient/src/app/admin/components/products/products.component.ts
@@ -1,49 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxSpinnerService } from "ngx-spinner";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Create_Product } from 'src/app/contracts/create_product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
+import { ListComponent } from './list/list.component';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
-
-  constructor(spinner:NgxSpinnerService,private httpClientService:HttpClientService) {
-    super(spinner)
-   }
-
-  ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallScaleMultiple)
-
-   /*  this.httpClientService.get({
-      controller:"Products"
-    }).subscribe(data=>console.log(data)); */
-
-  /*   this.httpClientService.post({
-      controller:"Products"
-    },{
-        Name:"Kalem",
-        Stock:15,
-        Price:10
-    }).subscribe(); */
-    /// getirmiyor post da atmıyor
-
-
-    /* this.httpClientService.put({
-      controller:"Products"
-    },{
-      id:"678f3175-0fe0-4e26-aa90-47c141a72c9e",
-      name:"Patates",
-      stock:1,
-      price:11
-
-    }).subscribe(); */
-
-  /*   this.httpClientService.delete({
-      controller:"Products"
-    },"a68e667e-8525-4721-8d81-3a55568aa419").subscribe(); */
-
+  constructor(
+    spinner: NgxSpinnerService,
+    private httpClientService: HttpClientService
+  ) {
+    super(spinner);
   }
 
+  ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallScaleMultiple);
+  }
+  @ViewChild(ListComponent) listComponents: ListComponent;
+  createdProduct(createdProduct: Create_Product) {
+    this.listComponents.getProducts();
+  }
 }
--- ETicaretClient/src/app/services/common/models/product.service.ts
+++ ETicaretClient/src/app/services/common/models/product.service.ts
@@ -1,31 +1,68 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Create_Product } from 'src/app/contracts/create_product';
+import { List_Products } from 'src/app/contracts/list_products';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private httpClientService: HttpClientService) {}
 
-  constructor(private httpClientService:HttpClientService) { }
+  create(
+    product: Create_Product,
+    successcallback?: () => void,
+    errorCallBack?: (errorMessage: string) => void
+  ) {
+    this.httpClientService
+      .post(
+        {
+          controller: 'Products',
+        },
+        product
+      )
+      .subscribe(
+        (result) => {
+          successcallback();
+        },
+        (errorResponse: HttpErrorResponse) => {
+          const _error: Array<{ key: string; value: Array<string> }> =
+            errorResponse.error;
+          let message = '';
+          _error.forEach((v, index) => {
+            v.value.forEach((_v, _index) => {
+              message += `${_v}<br>`;
+            });
+          });
 
-  create(product:Create_Product,successcallback?:any,errorCallBack?:any){
-    this.httpClientService.post({
-    controller:"Products"
-    },product)
-    .subscribe(result=>{
-      successcallback( );
-    },(errorResponse:HttpErrorResponse)=>{
-      const _error:Array<{key:string,value:Array<string>}>=errorResponse.error;
-      let message="";
-      _error.forEach((v,index)=>{
-         v.value.forEach((_v,_index)=>{
-          message += `${_v}<br>`;
-         });
-      });
+          errorCallBack(message);
+        }
+      );
+  }
 
-      errorCallBack(message);
-    });
+  async read(
+    page: number = 0,
+    size: number = 5,
+    successcallback?: () => void,
+    errorCallBack?: (errorMessage: string) => void
+  ): Promise<{ totalCount: number; products: List_Products[] }> {
+    const promiseData: Promise<{
+      totalCount: number;
+      products: List_Products[];
+    }> = this.httpClientService
+      .get<{ totalCount: number; products: List_Products[] }>({
+        controller: 'products',
+        queryString: `page=${page}&size=${size}`,
+      })
+      .toPromise();
+
+    promiseData
+      .then((d) => successcallback())
+      .catch((errorResponse: HttpErrorResponse) =>
+        errorCallBack(errorResponse.message)
+      );
+
+    return await promiseData;
   }
 }
